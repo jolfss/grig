72b1837fa2c3a65d88ed7aa151ac9af030b63547
Sean Brynjólfsson <seanbrynjolfsson@gmail.com>
2024-10-09 15:58:04 -0400

    Destroy visualize.py and move implementation to grig.py; refactors and final* beautification.

--- grig.py
+++ grig.py
@@ -1,5 +1,11 @@
 #!/home/pcgta/mambaforge/envs/dynamic_gaussians/bin/python
 """This modulescript encapsulates the core functionality of Grig."""
+#og imports
+import time
+import open3d as o3d
+from visualize import init_camera, render, rgbd2pcd 
+
+#
 from dataclasses import dataclass
 import torch
 import numpy as np
@@ -8,8 +14,11 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from typing import Dict, Tuple
 
+#-----------#
+#   types   #
+#-----------#
 @dataclass
-class Config:
+class GrigConfig:
     K:int
     "The number of means to cluster over."
     timestride:int
@@ -26,13 +35,7 @@ class Config:
     "Whether each feature component should be normalized.."
     color_mode:str
     """Which color mode to use for rendering.
-    Options:
-        CLUSTERS
-        POS
-        DPOS
-        ROT
-        DROT
-    """
+    Options: CLUSTER POS DPOS ROT DROT"""
 
 @dataclass
 class Parameters:
@@ -68,22 +71,28 @@ class Features:
     features : torch.Tensor
     """The output of some function on the input parameters.\n
     `(N, D*T//config.timestride)@cuda float`"""
+    
     is_fg : torch.Tensor
     """A boolean mask along gaussians specifying which are foreground and background.\n
     `(N, )@cuda bool`"""
+    
     pos : torch.Tensor
     """The centers of the gaussians (x,y,z).
     `(T,N,3)@cuda float`"""
+    
     rot : torch.Tensor
     """The orientations of the gaussians as normalized quaternions (qx,qy,qz,qw)\n
     NOTE: Have not fully considered the implications of the double cover of `SO(3)`.\n
     `(T,N,4)@cuda float`"""
+
     dpos_dt : torch.Tensor
     """The time derivative `(/s)` of `pos`.\n
     `(T,N,3)@cuda float`"""
+    
     drot_dt : torch.Tensor
     """The time derivative `(/s)` of `rot`.\n
     `(T,N,4)@cuda float`"""
+    
     T : int
     "The number of timesteps."
     N : int
@@ -91,7 +100,7 @@ class Features:
     D : int
     "The size of the feature dimension (at each timestep)."
 
-    def __init__(self, params : Parameters, config : Config):
+    def __init__(self, params : Parameters, config : GrigConfig):
         # initialize  
         self.pos = params.means3D
         self.rot = torch.nn.functional.normalize(params.unnorm_rotations, dim=-1)
@@ -136,14 +145,36 @@ class Features:
         """Forward item deletion."""
         del self.features[key]
 
+#----------------------#
+#   global constants   #
+#----------------------#
+# camera configuration
+w, h = 640, 360
+near, far = 0.01, 100.0
+view_scale = 3.9
+fps = 20
 
-#NOTE modified from [load_scene_data] in visualize.py 
+# clustering config
+default_config = GrigConfig(
+    timestride=1,
+    POS=1,
+    DPOS=1,
+    DROT=1,
+    K=24,
+    remove_bg=True,
+    normalize_features=True,
+    color_mode="CLUSTERS"
+)
+
+#-------------#
+#   methods   #
+#-------------#
 @torch.no_grad()
-def cluster(filepath_npz:str, config:Config) -> Tuple[Dict[str, torch.Tensor], Features, torch.Tensor, torch.Tensor]:
+def cluster(filepath_npz:str, config:GrigConfig) -> Tuple[Dict[str, torch.Tensor], Features, torch.Tensor, torch.Tensor]:
     """
-    The primary clustering algorithm `(param_filepath, config) -> (scene_data, features, labels, centers)`. 
+    The clustering algorithm `(param_filepath, config) -> (scene_data, features, labels, centers)`. 
     """
-
+    
     print(F"Opening {filepath_npz}")
     params = dict(np.load(filepath_npz))
     params = {k: torch.tensor(v).cuda().float() for k, v in params.items()}
@@ -210,5 +241,83 @@ def cluster(filepath_npz:str, config:Config) -> Tuple[Dict[str, torch.Tensor], F
 
     return scene_data, features, cluster_labels, cluster_centers
     
+def visualize(filepath_npz:str, config:GrigConfig):
+    scene_data, _, _, cluster_centers  = cluster(filepath_npz, config)
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(width=int(w * view_scale), height=int(h * view_scale), visible=True)
+
+    w2c, k = init_camera()
+    im, depth = render(w2c, k, scene_data[0])
+    init_pts, init_cols = rgbd2pcd(im, depth, w2c, k, show_depth=False)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = init_pts
+    pcd.colors = init_cols
+    vis.add_geometry(pcd)
+    
+    cluster_centers = []
+    for _ in range(config.K):
+        cluster_center = o3d.geometry.TriangleMesh.create_sphere(radius=0.005)  # Adjust the radius as needed
+        cluster_center.paint_uniform_color([1, 1, 1])  # Color the sphere red
+        cluster_centers.append(cluster_center)
+        vis.add_geometry(cluster_center)
+
+    view_k = k * view_scale
+    view_k[2, 2] = 1
+    view_control = vis.get_view_control()
+    cparams = o3d.camera.PinholeCameraParameters()
+    cparams.extrinsic = w2c
+    cparams.intrinsic.intrinsic_matrix = view_k
+    cparams.intrinsic.height = int(h * view_scale)
+    cparams.intrinsic.width = int(w * view_scale)
+    view_control.convert_from_pinhole_camera_parameters(cparams, allow_arbitrary=True)
+
+    render_options = vis.get_render_option()
+    render_options.point_size = view_scale
+    render_options.light_on = False
+    start_time = time.time()
+    num_timesteps = len(scene_data)
+    while True:
+        passed_time = time.time() - start_time
+        passed_frames = passed_time * fps
+        t = int(passed_frames % num_timesteps)
+
+        cam_params = view_control.convert_to_pinhole_camera_parameters()
+        view_k = cam_params.intrinsic.intrinsic_matrix
+        k = view_k / view_scale
+        k[2, 2] = 1
+        w2c = cam_params.extrinsic
+
+        # render gaussian splat
+        im, depth = render(w2c, k, scene_data[t])
+        pts, cols = rgbd2pcd(im, depth, w2c, k, show_depth=False)
+        pcd.points = pts  
+        pcd.colors = cols
+        vis.update_geometry(pcd)
+
+        #  centers visualization 
+        centers = torch.cat([cluster_centers[t],torch.ones((config.K,1))],dim=-1) # (K,4)
+        centers = centers.numpy() @ w2c.T
+        centers[:,:3] *= 0.25
+        centers = centers @ np.linalg.inv(w2c).T
+        
+        # update the position of each sphere based on the camera coordinates
+        for c in range(config.K):
+            cluster_centers[c].translate(centers[c,:3], relative=False)
+
+            # Update each sphere individually in the visualizer
+            vis.update_geometry(cluster_centers[c])
+
+        if not vis.poll_events():
+            break
+        vis.update_renderer()
+
+    vis.destroy_window()
+    del view_control
+    del vis
+    del render_options
+
+import sys
 if __name__ == "__main__":
-    ()
+    default_config.color_mode = sys.argv[2]
+    visualize(F"./output/pretrained/{sys.argv[1]}/params.npz", default_config)
